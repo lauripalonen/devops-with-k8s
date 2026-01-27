948862a91503b0dacd2bb1282f030c7954ace295
Lauri Palonen <lauri.palonen@futurice.com>
2026-01-27 16:21:14 +0200

    add fastapi, implement endpoint for current timestamp

--- log-output/README.md
+++
@@ -1,8 +0,0 @@
-# Log output
-Prints out a random string every 5 seconds.
-
-## Deployment instructions
-Pre-setup: ensure kubectl and k3d are installed and a cluster is running.
-
-1. Run `kubectl create deployment log-output --image=palolaur/log-output` to deploy.
-2. Run `kubectl logs -f log-output-<pod-id>` to display the logs.
--- log-output/main.py
+++ log-output/main.py
@@ -1,14 +1,31 @@
 import datetime
 import uuid
-import time
+import asyncio
+import logging
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.responses import HTMLResponse
 
-def main():
+logger = logging.getLogger("uvicorn.error")
+logs = []
+
+async def log_task():
     while True:
         curr_time = datetime.datetime.now().isoformat()
-        output_str = uuid.uuid8()
-        print(f"{curr_time}: {output_str}")
-        time.sleep(5)
+        output_str = uuid.uuid4()
+        log = f"{curr_time}: {output_str}"
+        logs.append(log)
+        logger.info(log)
+        await asyncio.sleep(5)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    task = asyncio.create_task(log_task())
+    yield
+    task.cancel()
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return logs[-1] if logs else "No logs yet"
